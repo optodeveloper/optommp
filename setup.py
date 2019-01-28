51f0af908556127bdebe7ec306301d601958c621
Terry Orchard <41756756+OPTO-torchard@users.noreply.github.com>
2019-01-28 12:58:33 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='optommp',
-        version='0.1',
+        version='0.2',
         description='Python toolkit for Opto 22 memory-mapped devices',
-        url='http://github.com/OPTO-torchard/optommp',
-        download_url='https://github.com/OPTO-torchard/optommp/archive/v0.1.tar.gz',
+        long_description="Python toolkit to access data on Opto memory-mapped devices, specifically written for use with the _groov_ EPIC PR1 processor. See [OptoMMP Protocol Guide](https://www.opto22.com/support/resources-tools/documents/1465-optommp-protocol-guide "opto22 support") (form 1465) for details.",
+        url='http://github.com/optodeveloper/optommp',
+        download_url='https://github.com/optodeveloper/optommp/archive/v0.2.tar.gz',
         license='mit',
         author='Opto 22',
         author_email='torchard@opto22.com',

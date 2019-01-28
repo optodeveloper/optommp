d350f2a2e14b2fd8cd13d1c568399a2b093e64da
Terry Orchard <developer@opto22.com>
2019-01-28 22:25:44 +0000

    Updated long_description

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(name='optommp',
-        version='0.2',
+        version='0.3',
         description='Python toolkit for Opto 22 memory-mapped devices',
-        long_description=long_description,
+        long_description="Python toolkit to access data on Opto 22 *groov* EPIC using the OptoMMP protocol. See http://developer.opto22.com and OptoMMP Protocol Guide for more details: https://www.opto22.com/support/resources-tools/documents/1465-optommp-protocol-guide (form 1465).",
         url='http://github.com/optodeveloper/optommp',
-        download_url='https://github.com/optodeveloper/optommp/archive/v0.2.tar.gz',
+        download_url='https://github.com/optodeveloper/optommp/archive/v0.3.tar.gz',
         license='mit',
         author='Opto 22',
         author_email='torchard@opto22.com',
